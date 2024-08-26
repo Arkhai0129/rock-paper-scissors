7d807b4eea02ca97e9ba6e0726801ba1be2a8d18
Arkhai0129 <arkhaib9@gmail.com>
2024-08-26 17:00:37 +0800

    Add files via upload

---
+++ precache-manifest.dac273166a7fb7764870c79c26066a51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164708c4d7c12a9ee5e42a4ca6281ad4",
+    "url": "/pro-react/index.html"
+  },
+  {
+    "revision": "f9c5f51cbaada7f3cbcf",
+    "url": "/pro-react/static/css/main.8848a576.chunk.css"
+  },
+  {
+    "revision": "57047fe85350774d405b",
+    "url": "/pro-react/static/js/2.2d18b849.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/pro-react/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "f9c5f51cbaada7f3cbcf",
+    "url": "/pro-react/static/js/main.9436d128.chunk.js"
+  },
+  {
+    "revision": "53e755f8c5152566dfe5",
+    "url": "/pro-react/static/js/runtime~main.9f231d72.js"
+  },
+  {
+    "revision": "450c068a9179b4874da248c8c5a32dea",
+    "url": "/pro-react/static/media/paper.450c068a.png"
+  },
+  {
+    "revision": "527d86e093f0c09b6d31680a6843094b",
+    "url": "/pro-react/static/media/rock.527d86e0.png"
+  },
+  {
+    "revision": "1342bdb2f2dd3713fe240438a57b5ea8",
+    "url": "/pro-react/static/media/scissors.1342bdb2.png"
+  }
+]);
